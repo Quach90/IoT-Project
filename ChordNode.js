854b30261f49f6c59f060f0ca99379ab267224d3
Simon <dakada@hotmail.com>
2015-02-17 12:04:13 +0100

    Added getRequest function

--- ChordNode.js
+++ ChordNode.js
@@ -3,20 +3,45 @@
  */
 var http = require('http');
 
-
-var node = ();
-
 var id;
 var successor;
 var predecessor;
 
-function join(node){
-    var currentNode = node;
+function join(port){
+    var currentNode = {
+        port: this.port,
+        id: getRequest(port, '/id'),
+        successorId: getRequest(port, '/successorid')
+    };
     while(true){
-        if(this.id > currentNode.id )
+        if(this.id > currentNode.id && this.id < currentNode.succId) {
+            predecessor = currentNode.id;
+            successor = currentNode.succId;
+            /* FIXME
+            Notify nodes!!
+             */
+            break;
+        }
     }
 }
 
-function getRequest(port){
+function getRequest(port, path) {
+    var options = {
+        host: '127.0.0.1',
+        path: path,
+        port: port,
+        method: 'GET'
+    };
 
+    callback = function (response) {
+        var str = '';
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+
+        response.on('end', function () {
+            return str;
+        });
+    };
+    return http.request(options, callback);
 }
