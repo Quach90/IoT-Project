6fb34efbf6e1834b1832dcf2b47bf6496bb80744
Joakim Quach <joakimquach@gmail.com>
2015-02-17 11:43:45 +0100

    hi

--- ChordNode.js
+++ ChordNode.js
@@ -1,4 +1,22 @@
 /**
  * Created by Joakim Quach on 16-02-2015.
  */
-var Node = "yeah";
+var http = require('http');
+
+
+var node = ();
+
+var id;
+var successor;
+var predecessor;
+
+function join(node){
+    var currentNode = node;
+    while(true){
+        if(this.id > currentNode.id )
+    }
+}
+
+function getRequest(port){
+
+}
