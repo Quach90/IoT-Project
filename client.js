0d646d220c49688119e7f14470b6d4e3387ea7fc
Simon <dakada@hotmail.com>
2015-03-05 08:16:14 +0100

    UI done

--- client.js
+++ client.js
@@ -8,6 +8,7 @@ $(function() {
 });
 
 var node;
+var hasResource = false;
 
 function getNode(port) {
     document.title = 'Node ' + port;
@@ -16,6 +17,7 @@ function getNode(port) {
     $("#lookupValue").val("");
     $("#resourceUrl").val("");
     $("#lookupResult").empty();
+    $("#graph").empty();
     $("#pred").empty();
     $("#succ").empty();
     $("#delete").empty();
@@ -30,6 +32,7 @@ function getNode(port) {
     $.get("http://127.0.0.1:" + port + "/getHasResource", function (data) {
         var result = JSON.parse(data);
         if(result.hasResource){
+            drawGraph();
             $('#resourceInfo').append("<button onclick='getResource()'>Get Spark State</button>");
             $('#resourceInfo').append("<table id='resourceTable'><tr><th>Time</th><th>Name</th><th>Data</th></tr></table>");
         }
@@ -118,6 +121,95 @@ function addResource(){
     $.post("http://127.0.0.1:" + node.port + "/postResource", resUrl, function(data){
         window.location.href = "http://localhost:8080/IoT%20Project/hi.html?port=" + node.port;
     })
+}
+
+function drawGraph(){
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var parseDate = d3.time.format("%d-%b-%y").parse;
+
+    var x = d3.time.scale()
+        .range([0, width]);
+
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var line = d3.svg.line()
+        .x(function (d) {
+            return x(d.timestamp);
+        })
+        .y(function (d) {
+            return y(d.data);
+        });
+
+    var svg = d3.select("#graph").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.json("http://127.0.0.1:" + node.port + "/getGraphData", function (error, data) {
+        data.forEach(function (d) {
+            delete d.name;
+            delete d._id;
+            d.timestamp = d.timestamp;//new Date(d.timestamp);
+            d.data = +d.data;
+        });
+        console.log(JSON.stringify(data))
+        x.domain(d3.extent(data, function (d) {
+            return d.timestamp;
+        }));
+        y.domain(d3.extent(data, function (d) {
+            return d.data;
+        }));
+
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Temperature - Celsius");
+
+        svg.append("path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", line);
+    });
+}
+
+function getUrlParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
 }
 
 function getUrlParameter(sParam)
